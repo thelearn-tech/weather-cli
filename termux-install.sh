30a325ba84a3503c2d80c02ea3c3057577c647ac
Pritam Behera <69230350+thelearn-tech@users.noreply.github.com>
2020-11-20 01:55:36 +0530

    Create termux-install.sh

---
+++ termux-install.sh
@@ -0,0 +1,17 @@
+printf '\033[32m';echo Script by thelearn-tech
+echo AKA Pritam Behera
+pkg install curl
+pkg upgrade
+clear
+cd /$HOME
+echo Enter name of your city
+read a
+cd $PREFIX/bin
+touch weather
+echo "curl wttr.in/$a" >> weather
+chmod +x weather
+printf '\033[32m';echo now type weather in
+echo in your terminal to see weather of
+echo your entered city $a
+sleep 1
+exit 1
